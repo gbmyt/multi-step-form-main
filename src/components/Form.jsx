7304faf5db4750b44613634d1adfff47576cd064
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-03-29 14:27:43 -0400

    Start stubbing out form fields boilerplate. No styling yet, but plan to begin with mobile styles first so have stubbed out some elements that will only appear in desktop view and commented them out for now.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,63 @@
+import { formStepsData } from "../data/data"
+import bgMobile from "../assets/images/bg-sidebar-mobile.svg"
+
 const Form = () => {
+    const steps = ["1", "2", "3", "4"]
+    // const formSteps = ["Your Info", "Select Plan", "Add-Ons", "Summary"]
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+        console.log(e.target, " clicked")
+    }
+
     return (
         <form id="form">
-            <label htmlFor="name">
-                Name:
-                <input type="text" name="name" />
-            </label>
+            {steps && steps.map((step, i) => <button key={i}>{step}</button>)}
+
+            <header>
+                <img src={bgMobile} alt="background-image" />
+                <h1>{formStepsData["1"].title}</h1>
+                <p>{formStepsData["1"].subTitle}</p>
+            </header>
+
+            {/* Only show this if in desktop view */}
+            {/* <section>
+                {formSteps &&
+                    formSteps.map((stepText, index) => (
+                        <>
+                            <h2>Step {index + 1}</h2>
+                            <p>{stepText}</p>
+                        </>
+                    ))}
+            </section> */}
+
+            <section>
+                form steps inputs todo
+                {/* {formFields &&
+                    formFields.map((fieldName) => (
+                        <>
+                            <label htmlFor={fieldName}>
+                                {fieldName}
+                                <input
+                                    required
+                                    type="text"
+                                    name={fieldName}
+                                    placeholder={fieldName}
+                                />
+                            </label>
+                        </>
+                    ))} */}
+            </section>
+
+            {/* Don't Show on First Page  */}
+            {/* <button type="submit" onClick={handleFormSubmit}>
+                Go Back
+            </button> */}
+
+            {/* Change to 'Confirm' on last page  */}
+            <button type="submit" onClick={handleFormSubmit}>
+                Next Step
+            </button>
         </form>
     )
 }
