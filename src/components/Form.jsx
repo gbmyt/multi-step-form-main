29080e984c818b628385864ab1b09b40d58e8745
Gabriela <48141726+gbmyt@users.noreply.github.com>
2024-03-29 13:15:51 -0400

    Added a Form component and imported for rendering within App

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+const Form = () => {
+    return (
+        <form id="form">
+            <label htmlFor="name">
+                Name:
+                <input type="text" name="name" />
+            </label>
+        </form>
+    )
+}
+
+export default Form
